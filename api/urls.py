8b407ac04ef14697c2157a00675abfc5e42043ef
Eman Ramadan <eman01148597034@gmail.com>
2024-04-16 06:25:17 +0200

    searchUpdate

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from search.views import direct_search,InDirect_search
 from eBook.views import *
+from search.views import searchAPI
 urlpatterns = [
     path('ebooks/', EbookAPI.as_view()),
     path('ebook_categories/', EbookCategoryAPI.as_view()),
     path('filter/', filter_books_by_category),
-    path('direct_search/', direct_search),
-    path('InDirect_search/', InDirect_search),
+    path('search/',searchAPI.as_view()),
 ]
