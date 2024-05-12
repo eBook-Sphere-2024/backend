124ab307cafc8f60201b97225fd944f14e446e8b
Eman Ramadan <eman01148597034@gmail.com>
2024-05-13 00:24:59 +0300

    edit data and default image profile

--- User/models.py
+++ User/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User as DjangoUser
 
 class UserProfile(models.Model):
     user= models.ForeignKey(DjangoUser, on_delete=models.CASCADE , related_name='user_profile')
-    profile_image = models.CharField(max_length=2000 , default='assets/profile.png')
+    profile_image = models.CharField(max_length=2000 , default='https://drive.google.com/thumbnail?id=1cCbs7SU4Ph8j3bnweJsouYqnkOkhlsiH')
 
     def __str__(self):
         return self.user.username
